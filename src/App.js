74d0ddd0e830d5d2ff3c182e56184ae295a62100
MD Shahin <esjgroupinc7@gmail.com>
2022-10-18 11:37:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import About from './components/About/About';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+import Orders from './components/Orders/Orders';
+import PrivateRoute from './components/routes/PrivateRoute';
+import Shipping from './components/shipping/Shipping';
+import Shop from './components/Shop/Shop';
+import { SignUp } from './components/SignUp/SignUp';
+import Main from './layouts/Main';
+import { productsAndCartLoader } from './loaders/productsAndCartLoader';
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('products.json'),
+          element: <Shop></Shop>
+        },
+        {
+          path:'orders',
+          loader: productsAndCartLoader,
+          element: <Orders></Orders>
+        },
+        {
+          path: 'inventory',
+          element: <PrivateRoute><Inventory></Inventory></PrivateRoute>
+        },
+        {
+          path: 'shipping',
+          element: <PrivateRoute><Shipping></Shipping></PrivateRoute>
+        },
+        {
+          path:'about',
+          element:<About></About>
+        },
+        {
+          path:'login',
+          element:<Login></Login>
+        },
+        {
+          path:'signup',
+          element:<SignUp></SignUp>
+        }
+      ]
+    },
+    
+  ])
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIuOI6mQF-tsJkUHr5rpF2Nee8EWQSo48",
+  authDomain: "ema-john-simple-4ed01.firebaseapp.com",
+  projectId: "ema-john-simple-4ed01",
+  storageBucket: "ema-john-simple-4ed01.appspot.com",
+  messagingSenderId: "404491665062",
+  appId: "1:404491665062:web:7fd896c005718d11a07e67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
